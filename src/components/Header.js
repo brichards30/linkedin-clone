bbfa0a9911d11237f2e2b36050517448b2bf5cde
Brittany Richards <brittany.m.richards3@gmail.com>
2021-11-12 15:58:26 -0600

    got the user and work menu up

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,6 +53,24 @@ const Header = (props) => {
                                 <span>Notifications</span>
                             </a>
                         </NavList>
+
+                        <User>
+                            <a>
+                                <img src="/images/user.svg" alt=""/>
+                                <span>Me</span>
+                                <img src="/images/down-icon.svg" alt=""/>
+                            </a>
+                        </User>
+
+                        <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt=""/>
+                                <span>
+                                    Work
+                                <img src="/images/down-icon.svg" alt=""/>
+                                </span>
+                            </a>
+                        </Work>
                     </NavListWrap>
                 </Nav>
             </Content>
@@ -187,6 +205,28 @@ const NavList = styled.li`
             }
         }
     }
+`;
+
+const User = styled(NavList)`
+a > svg {
+    width: 24px;
+    border-radius: 50%;
+}
+
+a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%; 
+}
+
+span {
+    align-items: center;
+}
+
+`;
+
+const Work = styled(User)`
+    border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
 
