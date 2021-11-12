a05f9d04b2f652c9bf24d9ee7ee79d0da472ea09
Brittany Richards <brittany.m.richards3@gmail.com>
2021-11-12 12:56:06 -0600

    working

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,8 +11,14 @@ const Login = (props) => {
                     <Join>
                         Join now
                     </Join>
+                    <SignIn>Sign in</SignIn>
                 </div>
             </Nav>
+            <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                </Hero>
+            </Section>
         </Container>
     )
 };
@@ -52,6 +58,60 @@ const Join = styled.a`
     color: rgba(0,0,0,0.9);
     text-decoration: none;
     }
+`;
+
+const SignIn = styled.a`
+    box-shadow: inset 0 0 0 1px #0a66c2;
+    color: #0a66c2;
+    border-radius: 24px;
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600px;
+    line-height: 40px;
+    padding: 10px 24px;
+    text-align: center;
+    background-color: rgba(0,0,0,0);
+    &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+    }
+`;
+
+const Section = styled.section`
+    display: flex;
+    align-content: start;
+    min-height: 700px;
+    padding-bottom: 138px;
+    padding-top: 40px;
+    padding: 60px 0;
+    position: relative;
+    flex-wrap: wrap;
+    width: 100%;
+    max-width: 1128px;
+    align-items: center;
+    margin: auto;
+    @media (max-width: 768px) {
+        margin: auto;
+        min-height: 0px;
+    } 
+`;
+
+const Hero = styled.div`
+    width: 100%;
+    h1 {
+        padding-bottom:0;
+        width: 55%;
+        font-size: 56px;
+        color: #2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width: 768px) {
+        text-align: center;
+        font-size: 20px;
+        width: 100%;
+        line-height: 2;
+        }
 `;
 
 export default Login;
