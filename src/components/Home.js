a1802bacd29be26e3034ea5954eef8e74cae0750
Brittany Richards <brittany.m.richards3@gmail.com>
2021-11-15 13:50:43 -0600

    leftside, main, rightside

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,25 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Rightside from "./Rightside";
+import Main from "./Main";
 
 const Home = (props) => {
     return (
-    <Container>
-        <Section>
-            <h5>
-                <a>Hiring in a hurry? - </a>
-            </h5>
-            <p>
-                 Find talented pros in record time with Upwork and keep business moving.
-            </p>
-        </Section>
-        <Layout>
-            <div>Left Side</div>
-            <div>Main</div>
-            <div>Right Side</div>
-        </Layout>
-    </Container>
+        <Container>
+            <Section>
+                <h5>
+                    <a>Hiring in a hurry? - </a>
+                </h5>
+                <p>
+                    Find talented pros in record time with Upwork and keep business moving.
+                </p>
+            </Section>
+            <Layout>
+                <Leftside/>
+                <Main/>
+                <Rightside/>
+            </Layout>
+        </Container>
     )
 };
 
