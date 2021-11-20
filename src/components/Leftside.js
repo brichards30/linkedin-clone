629d092bb2dc96740d8d09a39e6cbee94a3a4140
Brittany Richards <brittany.m.richards3@gmail.com>
2021-11-20 16:46:40 -0600

    added rest of images to project. added artcard styling

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -13,6 +13,11 @@ const Leftside = (props) => {
                         <AddPhotoText>Add a photo</AddPhotoText>
                     </a>
                 </UserInfo>
+                <Widget>
+                    <a>
+                        <div></div>
+                    </a>
+                </Widget>
             </ArtCard>
         </Container>
 
@@ -50,10 +55,35 @@ const CardBackground = styled.div`
     margin: -12px -12px 0;
 `;
 
-const Photo = styled.div``;
+const Photo = styled.div`
+    box-shadow: none;
+    background-image: url("/images/photo.svg");
+    width: 72px;
+    height: 72px;
+    box-sizing: border-box;
+    background-clip: content-box;
+    background-color: white;
+    background-position: center;
+    background-size: 60%;
+    background-repeat: no-repeat;
+    border: 2px solid white;
+    margin: -38px auto 12px;
+    border-radius: 50%;
+`;
 
-const Link = styled.div``;
+const Link = styled.div`
+    font-size: 16px;
+    line-height: 1.5;
+    color: rgba(0, 0, 0, 0.9);
+    font-weight: 600;
+`;
 
-const AddPhotoText = styled.div``;
+const AddPhotoText = styled.div`
+    color: #0a66c2;
+    margin-top: 4px;
+    font-size: 12px;
+    line-height: 1.33;
+    font-weight: 400;
+`;
 
 export default Leftside;
