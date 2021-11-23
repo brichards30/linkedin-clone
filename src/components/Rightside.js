538c2a20d9eebec8ab07f14ee104a3856ce2b495
Brittany Richards <brittany.m.richards3@gmail.com>
2021-11-23 12:13:10 -0600

    right side complete, working on main share card

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -4,7 +4,6 @@ const Rightside = (props) => {
     return (
         <Container>
             <FollowCard>
-
                 <Title>
                     <h2>Add to your feed</h2>
                     <img src="/images/feed-icon.svg" alt=""/>
@@ -13,7 +12,7 @@ const Rightside = (props) => {
                 <FeedList>
                     <li>
                         <a>
-                            <Avatar />
+                            <Avatar/>
                         </a>
                         <div>
                             <span>#Linkedin</span>
@@ -22,16 +21,23 @@ const Rightside = (props) => {
                     </li>
                     <li>
                         <a>
-                            <Avatar />
-                            <div>
-                                <span>#Video</span>
-                                <button>Follow</button>
-                            </div>
+                            <Avatar/>
                         </a>
+                        <div>
+                            <span>#Video</span>
+                            <button>Follow</button>
+                        </div>
                     </li>
                 </FeedList>
-
+                
+                <Recommendation>
+                    View all recommendations
+                    <img src="/images/right-icon.svg" alt=""/>
+                </Recommendation>
             </FollowCard>
+            <BannerCard>
+                <img src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg" alt=""/>
+            </BannerCard>
         </Container>
     );
 };
@@ -62,39 +68,59 @@ const Title = styled.div`
 `;
 
 const FeedList = styled.ul`
-    margin-top: 16px;
-    li {
-        display: flex;
-        align-items: center;
-        margin: 12px 0;
-        position: relative;
-        font-size: 14px;
-        & > div {
-            display: flex;
-            flex-direction: column;
-        }
-        
-        button {
-            background-color: transparent;
-            color: rgba(0, 0, 0, 0.6);
-            box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
-            padding: 16px;
-            align-items: center;
-            border-radius: 15px;
-            box-sizing: border-box;
-            font-weight: 600;
-            display: inline-flex;
-            justify-content: center;
-            max-height: 32px;
-            max-width: 480px;
-            text-align: center;
-            outline: none;
-        }
+  margin-top: 16px;
+  li {
+    display: flex;
+    align-items: center;
+    margin: 12px 0;
+    position: relative;
+    font-size: 14px;
+    
+    & > div {
+      display: flex;
+      flex-direction: column;
     }
+    
+    button {
+      background-color: transparent;
+      color: rgba(0, 0, 0, 0.6);
+      box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      align-items: center;
+      border-radius: 15px;
+      box-sizing: border-box;
+      font-weight: 600;
+      display: inline-flex;
+      justify-content: center;
+      max-height: 32px;
+      max-width: 480px;
+      text-align: center;
+      outline: none;
+    }
+  }
 `;
 
 const Avatar = styled.div`
-    background-image: 
+    background-image: url("https://static-exp1.licdn.com/sc/h/1b4vl1r54ijmrmcyxzoidwmxs");
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 48px;
+    height: 48px;
+    margin-right: 8px;
 `;
 
+const Recommendation = styled.a`
+    color: #0a66c2;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+`;
+
+const BannerCard = styled(FollowCard)`
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
 export default Rightside;
