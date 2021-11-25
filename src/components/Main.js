ab625c62b5fb1b8ccc400914d0a90321d55f8f2a
Brittany Richards <brittany.m.richards3@gmail.com>
2021-11-25 11:39:09 -0600

    need to resize buttons for photo, event, article

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,16 +3,28 @@ import styled from 'styled-components';
 const Main = (props) => {
     return (
         <Container>
-            <ShareBox>Share</ShareBox>
-            <div>
-                <img src="/images/user.svg" alt=""/>
-                <button>Start a post</button>
-            </div>
-            <div>
-                <button>
-                    <img src="" alt=""/>
-                </button>
-            </div>
+            <ShareBox>Share
+                <div>
+                    <img src="/images/user.svg" alt=""/>
+                    <button>Start a post</button>
+                </div>
+                <div>
+                    <button>
+                        <img src="/images/photo-svgrepo-com.svg" alt=""/>
+                        <span>Photo</span>
+                    </button>
+
+                    <button>
+                        <img src="/images/event-svgrepo-com.svg" alt=""/>
+                        <span>Event</span>
+                    </button>
+
+                    <button>
+                        <img src="/images/paper-svgrepo-com.svg" alt=""/>
+                        <span>Write article</span>
+                    </button>
+                </div>
+            </ShareBox>
         </Container>);
 };
 
@@ -37,6 +49,17 @@ const ShareBox = styled(CommonCard)`
     color: #958b7b;
     margin: 0 0 8px;
     background: white;
+    div {
+        button {
+            outline: none;
+            color: rgba(0, 0, 0, 0.6);
+            font-size: 14px;
+            line-height: 1.5;
+            min-height: 48px;
+            background: transparent;
+            
+        }
+    }
 `;
 
 export default Main;
